cc0e999a65d5d85d676a3df45ae3a2763b7d8203
Nathan McElwain <nathanmcelwain@ymail.com>
2016-12-02 11:23:40 -0800

    Commit 12/2/16

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,45 @@
-import React, {Component, PropTypes} from 'react';
-import {createContainer} from 'meteor/react-meteor-data';
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
 
-import {Tasks} from '../api/tasks.js';
+import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
 
   renderTasks() {
-    return this.props.tasks.map((task) =>(
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task =>  !task.checked)
+    }
+    return filteredTasks.map((task) =>(
       <Task key={task._id} task={task} />
     ));
   }
@@ -17,8 +48,22 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-            <h1>Stuff I Gotta Do</h1>
+          <h1>Stuff I Gotta Do</h1>
+
+          <label className="hide-completed">
+            <input type="checkbox" readOnly checked={this.state.hideCompleted} onClick={this.toggleHideCompleted.bind(this)} />
+            Hide Completed Tasks
+          </label>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
+
         <ul>
           {this.renderTasks()}
         </ul>
@@ -34,6 +79,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 }, App);
